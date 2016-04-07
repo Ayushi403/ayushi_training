8f1c6df8dd7d49d23a98a8f57c0deea090f5a47b
Ayushi403 <ayushi.gupta@traveltriangle.com>
2016-04-07 17:52:01 +0530

    Added cart in the sidebar

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -42,11 +42,10 @@ class LineItemsController < ApplicationController
   def create
    @cart = current_cart
   product = Product.find(params[:product_id])
-  @line_item = @line_item = @cart.add_product(product.id)
+  @line_item = @cart.add_product(product.id)
   respond_to do |format|
     if @line_item.save
-      format.html { redirect_to @line_item.cart,
-      notice: 'Line item was successfully created.' }
+      format.html { redirect_to store_url }
       format.json { render json: @line_item,
       status: :created, location: @line_item }
     else
