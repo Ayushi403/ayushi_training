63b7c7ea9d035c16ecbbe7b1e71c7d894bc76d49
Ayushi403 <ayushi.gupta@traveltriangle.com>
2016-05-09 23:05:38 +0530

    Assocation for role user

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
 	before_filter :authenticate_user!
   def index
+  	binding.pry
   	@products = Product.order(:title)
   	@cart = current_cart
   end
---
+++ depot/app/models/roleuser.rb
@@ -0,0 +1,4 @@
+class Roleuser < ActiveRecord::Base
+  belongs_to :role
+  belongs_to :user
+end
