789021f9c3147db9fff3119853e60a30a6c0ec75
Ayushi403 <ayushi.gupta@traveltriangle.com>
2016-05-14 22:57:22 +0530

    Squashed commit of the following:
    commit ab4557ae97b5e248c18563ae12f9094152251b97
    Author: Ayushi403 <ayushi.gupta@traveltriangle.com>
    Date:   Sat May 14 22:56:01 2016 +0530
    
        Payment status,order summary and rating

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -5,8 +5,23 @@ class Order < ActiveRecord::Base
 	validates :pay_type, inclusion: PAYMENT_TYPES
 	def add_line_items_from_cart(cart)
 		cart.line_items.each do |item|
-			item.cart_id = nil
 			line_items << item
 		end
 	end
+
+
+	def self.total_amount
+		order_done = Order.where(:payment_status =>'done')
+		#total = Order.sum('total_price')
+		total = order_done.sum(:total_price)
+		total
+	end
+
+	def self.pending_amount
+		order_done = Order.where(:payment_status =>'pending')
+		#total = Order.sum('total_price')
+		total = order_done.sum(:total_price)
+		total
+	end
 end
+
