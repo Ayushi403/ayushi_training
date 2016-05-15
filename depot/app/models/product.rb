aa44fee9f840927579a6cf9af865777f581e71db
Ayushi403 <ayushi.gupta@traveltriangle.com>
2016-05-15 23:32:12 +0530

    validation on image

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -3,11 +3,12 @@ validates :title, :description, :image_url, presence: true
 #validates :title, :description, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
-# validates :image_url, format: {
-# with:
-# %r{\.(gif|jpg|png)$}i,
-# message: 'must be a URL for GIF, JPG or PNG image.'
-# }
+validates :image_url, format: {
+with:
+%r{\.(gif|jpg|png)$}i,
+message: 'must be a URL for GIF, JPG or PNG image.'
+}
+validates_inclusion_of :rating, :in => 1..5
 has_many :line_items
 has_many :orders, through: :line_items
 before_destroy :ensure_not_referenced_by_any_line_item
