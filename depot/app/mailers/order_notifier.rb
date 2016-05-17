9f167a8f12ffd0d560f6497afb8b8bbfcc585e8d
Ayushi403 <ayushi.gupta@traveltriangle.com>
2016-05-17 14:56:47 +0530

    send mail done

--- depot/app/mailers/order_notifier.rb
+++ depot/app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
- default from: 'pine2798@gmail.com'
+ default from: 'shop@store.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
